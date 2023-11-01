c2626050693d37ffb00f69a592421a466277c23e
GlebDev343 <gleb.wasd@gmail.com>
2023-11-02 02:40:47 +0300

    Add MeterModel model

--- main/admin.py
+++ main/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Manufacturer
+from .models import Manufacturer, MeterModel
 
 admin.site.register(Manufacturer)
+admin.site.register(MeterModel)
--- main/models.py
+++ main/models.py
@@ -4,3 +4,12 @@ from django.db import models
 class Manufacturer(models.Model):
     name = models.CharField(unique=True)
     contact_details = models.CharField()
+
+
+class MeterModel(models.Model):
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
+    model_name = models.CharField()
+    scale_size = models.IntegerField()
+
+    class Meta:
+        unique_together = ["manufacturer", "model_name"]
